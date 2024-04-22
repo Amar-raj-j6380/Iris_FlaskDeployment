75f5ea94ebdfc39a54671e96855bc803f110e3be
Amar-raj-j6380 <83690637+Amar-raj-j6380@users.noreply.github.com>
2024-04-22 21:08:30 +0530

    test2
    nkk

--- Flask_deployment/test_flask_app.py
+++ Flask_deployment/test_flask_app.py
@@ -9,7 +9,7 @@ def client():
 
 def test_home(client):    
     resp = client.get('/ping')
-    assert resp.json=={"message":"Hi there, I'm working!!"}
+    assert resp.json=={"yes"}
 
 def test_predict(client):
     test_data={'Gender':"Male", 'Married':"Unmarried",'Credit_History' : "Unclear Debts",'ApplicantIncome':100000,'LoanAmount':2000000}
